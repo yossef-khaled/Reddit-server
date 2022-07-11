501e49e4cbebde966f85afa2419bc8758f1e319d
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-11 23:37:31 +0200

    Add query action to get a specific post

--- src/resolvers/postResolver.ts
+++ src/resolvers/postResolver.ts
@@ -1,13 +1,26 @@
+//Import entities
 import { Post } from "../entities/Post";
+
+//Improt types
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+
+//Import from type-graphql 
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
 
-    @Query(() => [Post]) //** () => String ** is how you define waht the function returns  
-    posts(@Ctx() ctx : MyContext) {
-        return ctx.em.find(Post, {});
+    @Query(() => [Post]) //** () => String ** is how you define what the function returns  
+    posts(@Ctx() { em } : MyContext): Promise<Post[]> {
+        return em.find(Post, {});
+    }
+
+    @Query(() => Post, { nullable: true })
+    post(
+        @Arg('id', () => Int) id: number,
+        @Ctx() { em } : MyContext
+    ): Promise<Post | null> {
+        return em.findOne(Post, { id });
     }
 
 }
