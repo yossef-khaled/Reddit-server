0a52e16f796eb8a5ac967c92e02d7e72388912dc
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-11 20:47:00 +0200

    Setup the graphql end points with apollo

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,22 @@ import { Post } from './entities/Post';
 //Import micro configuration
 import microConfig from './mikro-orm.config'; 
 
+//Import express
+import express from 'express';
+
+//Import from apollo
+import { ApolloServer } from 'apollo-server-express';
+
+//Import from graphql
+import { buildSchema } from 'type-graphql';
+
+//Import resolvers
+import { HelloResolver } from './resolvers/helloResolver';
+import { PostResolver } from './resolvers/postResolver';
+
+//Import reflect-metadata
+import 'reflect-metadata';
+
 const main = async () => {
 
     const orm = await MikroORM.init(microConfig);
@@ -18,13 +34,32 @@ const main = async () => {
     const generator = orm.getSchemaGenerator();
     await generator.updateSchema();
 
-    //Ignore the error happening in the following line, just TS trusting issues
-    const post = orm.em.create(Post, {title: 'My first post'});
-    await orm.em.persistAndFlush(post);
+    const app = express();
 
-    //See all the posts out there
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false           
+        }),
+        context: () => ({ em : orm.em })
+    });
+
+    await apolloServer.start();
+
+    //Create graphql end point with apollo
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(3000, () => {
+        console.log('RUNNING ON PORT 3000....');
+    })
+
+    //Ignore the error happening in the following line, just TS trusting issues
+    // const post = orm.em.create(Post, {title: 'My first post'});
+    // await orm.em.persistAndFlush(post);
+
+    //See all the posts out there in the db
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
 
     console.log('Hello world from TS');   
 }
