da241f33c42014d9bb8bdd701d75a0071fa61dc2
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-11 07:13:08 +0200

    Start with micro orm and postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello world from TS');
+//Import from @mikro-orm
+import { MikroORM } from '@mikro-orm/core';
+
+//Import consts
+import { __prod__ } from './constants';
+
+//Import entities
+import { Post } from './entities/Post';
+
+//Import micro configuration
+import microConfig from './mikro-orm.config'; 
+
+const main = async () => {
+
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+
+    const generator = orm.getSchemaGenerator();
+    await generator.updateSchema();
+
+    //Ignore the error happening in the following line, just TS trusting issues
+    const post = orm.em.create(Post, {title: 'My first post'});
+    await orm.em.persistAndFlush(post);
+
+    //See all the posts out there
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+
+    console.log('Hello world from TS');   
+}
+
+main().catch((err) => {
+    console.log(err);
+});
