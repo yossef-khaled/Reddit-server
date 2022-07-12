129c53a351e8ea2178f5fc863bbef2a2304763a4
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-12 06:23:19 +0200

    Create an entity for user

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { PrimaryKey, Property, Entity } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+
+    @Field()
+    @PrimaryKey()
+    id!: number;
+
+    @Field(() => String)
+    @Property({type: 'date'})
+    createdAt = new Date();
+
+    @Field(() => String)
+    @Property({type: 'date', onUpdate: () => new Date() })
+    updatedAt = new Date();
+
+    @Field() //@Field() decorator declares whether this property is a db property or not. If it is not, then it will be hidden from graphql schema
+    @Property({ type: 'text', unique: true })
+    username!: string;
+
+    @Property({ type: 'text' })
+    password!: string;
+
+}
