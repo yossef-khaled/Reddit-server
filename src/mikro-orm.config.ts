0a40bc9ad58cd57923548626d378334fac571026
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-12 06:23:54 +0200

    Add user entity to create a user table in the db

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,14 @@
+//Import from micro-orm
 import { Options } from "@mikro-orm/core";
+
+//Import constants
 import { __prod__ } from "./constants";
+
+//Import entities
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
+
+//Import other stuff
 import path from "path";
 
 export default {
@@ -8,7 +16,7 @@ export default {
         pathTs: path.join(__dirname, './migrations'), // path to the folder with TS migrations (if used, we should put path to compiled files in `path`)
         glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'reddit',
     debug: !__prod__,
     type: 'postgresql',
