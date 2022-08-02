0543831797058edb097f709b6e041877dffb93ef
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-02 10:12:48 +0200

    Add some validator function

---
+++ src/utils/validators.ts
@@ -0,0 +1,58 @@
+import { UsernamePasswordInput } from "src/resolvers/UsernamePasswordInput";
+
+export function validateEmail(e: string) {
+
+    let emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailRegEx.test(String(e).toLowerCase())) { 
+        return {
+            message: 'Valid E-mail'
+        }; 
+    }
+
+    return null;
+}
+
+export function validateRegister(options: UsernamePasswordInput) {
+
+    if(options.email.length < 5) {
+
+        return [
+            {
+                field: 'email',
+                message: 'E-mail is too short.'
+            }
+        ]
+    }
+
+    if(validateEmail(options.email)) {
+        if (!validateEmail(options.email)) {
+            return [
+                {
+                    field: 'email',
+                    message: 'Invalid E-mail.'
+                }
+            ]
+        } 
+    }
+
+    if(options.username.length < 2) {
+        return [
+            {
+                field: 'username',
+                message: 'Username must be at least 2 charachters.'
+            }
+        ]
+    }
+
+    if(options.password.length < 3) {
+        return [
+            {
+                field: 'password',
+                message: 'password must be at least 3 charachters.'
+            }
+        ]
+    }
+
+    return null;
+}
