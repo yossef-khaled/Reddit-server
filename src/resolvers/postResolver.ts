5872f3ea293e059812a712d27f30b893a84201e1
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-16 13:12:49 +0200

    Make a query nyself instead of query builder tobe able to add a creator field

--- src/resolvers/postResolver.ts
+++ src/resolvers/postResolver.ts
@@ -2,7 +2,7 @@
 import { Post } from "../entities/Post";
 
 //Import from type-graphql 
-import { Arg, Ctx, Field, InputType, Int, Mutation, ObjectType, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, Field, FieldResolver, InputType, Int, Mutation, ObjectType, Query, Resolver, Root, UseMiddleware } from "type-graphql";
 import { sleep } from "../utils/sleep";
 import { MyContext } from "src/types";
 
@@ -31,29 +31,92 @@ class CreatePostReturn {
     post?: Post;
 }
 
-@Resolver()
+@ObjectType()
+class PaginatedPosts {
+    @Field(() => [Post])
+    posts: Post[];
+
+    @Field(() => Boolean)
+    hasMore: boolean;
+}
+
+@Resolver(Post)
 export class PostResolver {
+    @FieldResolver(() => String)
+    textSnippet(
+        @Root() post: Post
+    ) {
+        if(post.text.length < 50) {
+            return post.text;
+        }
+
+        else {
+            return `${post.text.slice(0, 50)}...`;
+        }
+        
+    }
+
 
     //Query decorator is for getting data
-    @Query(() => [Post]) //** () => String ** is how you define what the function returns  
+    @Query(() => PaginatedPosts) //** () => String ** is how you define what the function returns  
     async posts(
         @Arg('limit', () => Int) limit: number,
         @Arg('cursor', () => String, { nullable: true }) cursor: string | null
-    ): Promise<Post[]> {
+    ): Promise<PaginatedPosts> {
 
-        const safeLimit = Math.min(150, limit);
-        // await sleep(3000);
-        const qb = redditCloneDataSource
-        .getRepository(Post)
-        .createQueryBuilder("getPosts")
-        .orderBy('"createdAt"', 'DESC') // need to wrap column with douple quots for capitale case 
-        .take(safeLimit)
+        const realLimit = Math.min(10, limit);
+        const realLimitPlusOne = realLimit + 1;
+
+        const replacements: (number | object)[] = [realLimitPlusOne]
 
         if(cursor) {
-            qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+            replacements.push(new Date(parseInt(cursor)));
         }
 
-        return qb.getMany();
+        const posts = await redditCloneDataSource.query(
+            `
+                select p.*, 
+                json_build_object(
+                    'id', u.id,
+                    'username', u.username,
+                    'email', u.email,
+                    'createdAt', u."createdAt",
+                    'updatedAt', u."updatedAt"
+                ) creator
+                from post p 
+                inner join public.user u on u.id = p."creatorId"
+                ${cursor ? `where p."createdAt" < $2` : ''}
+                order by p."createdAt" DESC
+                limit $1
+            `
+        , replacements)
+        .catch((error) => console.log(error)); 
+
+        console.log(posts);
+
+        // const qb = redditCloneDataSource
+        // .getRepository(Post)
+        // .createQueryBuilder("p")
+        // .innerJoinAndSelect(
+        //     "p.creator",
+        //     "creator",
+        //     'creator.id = p."creatorId"' // notice the douple quotes around creatorId for camel-case
+        // )
+        // .orderBy('p."createdAt"', 'DESC') // need to wrap column with douple quotes for capitale case
+        // .take(realLimitPlusOne)
+
+        // if(cursor) {
+        //     qb.where('p."createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+        // }
+
+        // const posts = await qb.getMany();
+        const hasMore =  posts.length === realLimitPlusOne;
+        
+        posts.pop();
+        return {
+            posts,
+            hasMore
+        };
     }
 
     //Query decorator is for getting data
