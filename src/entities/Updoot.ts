18db53fdf5f4eb875f9c68a221451d9cc89cc54a
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-21 14:29:38 +0200

    Add an entity called updoot for votes

---
+++ src/entities/Updoot.ts
@@ -0,0 +1,30 @@
+import { Entity, BaseEntity, ManyToOne, PrimaryColumn, Column } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+import { User } from "./User";
+import { Post } from "./Post";
+
+@ObjectType()
+@Entity()
+export class Updoot extends BaseEntity {
+
+    @Field()
+    @Column({ type: 'int' })
+    value: number;
+
+    @Field()
+    @PrimaryColumn()
+    userId!: number;
+
+    @Field(() => User)
+    @ManyToOne(() => User, user => user.updoots)
+    user: User;
+
+    @Field()
+    @PrimaryColumn()
+    postId!: number;
+
+    @Field(() => Post)
+    @ManyToOne(() => Post, post => post.updoots)
+    post: Post;
+
+}
