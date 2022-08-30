8e7f7c183313085cf48c92773045cdc3ee644701
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-30 23:11:03 +0200

    Add the DataLoader in our context

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express"
 import { Redis } from "ioredis";
+import { createUsersLoader } from "./utils/createUsersLoader";
 
 export type MyContext = {
     redis: Redis;
     req: Request;
     res: Response;
+    userLoader: ReturnType<typeof createUsersLoader>;
 }
